ecf7b068c79c82bfeb8431ff7649e3e21386faf5
arminmaurice <armin.maurice2@gmail.com>
2014-10-12 02:19:42 -0400

    sign up

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Armin Maurice"
+    email    "arminmaurice@hotmail.com"
+    password "devils67"
+    password_confirmation "devils67"
+  end
+end
